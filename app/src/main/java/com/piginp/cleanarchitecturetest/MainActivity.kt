c122f465d43cac9f0f8f0394c984400d7703b80b
pigrist <pavel.pigin@gmail.com>
2022-09-07 20:24:03 +0300

    Initial commit

---
+++ app/src/main/java/com/piginp/cleanarchitecturetest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.piginp.cleanarchitecturetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
